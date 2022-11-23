e4fbad927a7509d4706534f88c50b92d08baca40
TanvirAnzum <tanviranzum14@gmail.com>
2022-11-23 23:51:02 +0600

    add jwt

--- index.js
+++ index.js
@@ -25,8 +25,45 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+// default server homepage
+
 app.get("/", (req, res) => {
   res.send("Welcome to the sell your pc server");
+});
+
+// middlewares function to verify jwt token
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({
+      message: "unauthorized access",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({
+        message: "Forbidden Access",
+      });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+// create jwt token
+
+app.post("/jwt", (req, res) => {
+  const data = req.body;
+  const token = jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.send({ token });
 });
 
 app.listen(port, () => {
